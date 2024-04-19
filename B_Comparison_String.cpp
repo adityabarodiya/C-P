eabf12708f47cc60458e626b9080dfde1aa4f08d
Aditya Barodiya <98759220+adityabarodiya@users.noreply.github.com>
2024-04-19 16:32:00 +0530

    first commit

---
+++ B_Comparison_String.cpp
@@ -0,0 +1,63 @@
+/*
+    Author - Aditya Barodiya
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+
+#define vi vector<int>
+
+#define println(a) cout << a << endl;
+#define print(a) cout << a << " ";
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+
+#define pb push_back
+#define printArr(a)       \
+    for (auto x : a)      \
+        cout << x << " "; \
+    cout << endl
+
+#define all(x) x.begin(), x.end()
+
+/*
+    Start Solving from here ---------------------------------------------------------------------
+*/
+
+void inputArr(vi &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        int ele;
+        cin >> ele;
+        v[i] = ele;
+    }
+}
+
+void solve()
+{
+    
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    // for fast input and output
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    clock_t z = clock();
+
+    int t = 0;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
