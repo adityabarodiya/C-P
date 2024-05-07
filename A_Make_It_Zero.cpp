74aaff2ac27f357fe09c78830ae84ac5a6d41052
Aditya Barodiya <98759220+adityabarodiya@users.noreply.github.com>
2024-05-07 13:11:43 +0530

    cp-900-4

---
+++ A_Make_It_Zero.cpp
@@ -0,0 +1,82 @@
+/*
+    Author - Aditya Barodiya
+
+    LinkedIn - https://www.linkedin.com/adityabarodiya
+    Github - https://github.com/adityabarodiya
+    Codeforces - https://codeforces.com/profile/adityabarodiya
+    CodeChef - https://www.codechef.com/users/adityabarodiya
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+
+#define vi vector<int>
+
+#define pl(a) cout << a << endl;
+#define p(a) cout << a << " ";
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define el cout << "\n";
+#define pb push_back
+#define p(a, b) cout << a << " " << b << endl;
+#define printArr(a)       \
+    for (auto x : a)      \
+        cout << x << " "; \
+    cout << endl
+
+#define all(x) x.begin(), x.end()
+
+/*
+    Start Solving from here ---------------------------------------------------------------------
+*/
+int SUM = 0;
+void inputArr(vi &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        int ele;
+        cin >> ele;
+
+        v[i] = ele;
+        SUM += v[i];
+    }
+}
+
+bool isOdd(int n)
+{
+    return (n & 1) == 1;
+}
+void solve()
+{
+    int a, b, n;
+    cin >> a >> b >> n;
+    vi v(n);
+    inputArr(v);
+    int c = b;
+    int ans = b;
+    for (int i : v)
+    {
+        ans += min(a-1,i);
+    }
+
+    pl(ans)
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    clock_t z = clock();
+
+    int t = 0;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        SUM = 0;
+    }
+    return 0;
+}
