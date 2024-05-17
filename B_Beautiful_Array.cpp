9b5e5000522389557e5a37d131cb3b0bc390fa11
Aditya Barodiya <98759220+adityabarodiya@users.noreply.github.com>
2024-05-17 23:07:49 +0530

    cf-945-A

---
+++ B_Beautiful_Array.cpp
@@ -0,0 +1,101 @@
+/*
+    Author - Aditya Barodiya
+
+    LinkedIn - https://www.linkedin.com/adityabarodiya
+    Github - https://github.com/adityabarodiya
+    Codeforces - https://codeforces.com/profile/adityabarodiya
+    CodeChef - https://www.codechef.com/users/adityabarodiya
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+
+#define vi vector<int>
+
+#define pl(a) cout << a << endl;
+#define p(a) cout << a << " ";
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define el cout << "\n";
+#define pb push_back
+#define p(a, b) cout << a << " " << b << endl;
+#define printArr(a)       \
+    for (auto x : a)      \
+        cout << x << " "; \
+    cout << endl
+
+#define all(x) x.begin(), x.end()
+
+/*
+    Start Solving from here ---------------------------------------------------------------------
+*/
+int SUM = 0;
+void inputArr(vi &v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        int ele;
+        cin >> ele;
+
+        v[i] = ele;
+        SUM += v[i];
+    }
+}
+
+bool isOdd(int n)
+{
+    return (n & 1) == 1;
+}
+void solve()
+{
+    int n, k, b, s;
+    cin >> n >> k >> b >> s;
+
+    vi v(n);
+    s -= f;
+    if (s < 0)
+    {
+        pl(-1);
+        return;
+    }
+    int t = k - 1;
+    if ((t * (n - 1) < rem) || k * b > s)
+    {
+        pl(-1); return;
+    }
+    vi ans(0);
+    ans.pb(f);
+    int c = rem / t;
+    while (c--)
+        ans.pb(t);
+
+    if (rem % t != 0)
+        ans.pb(rem % t);
+    int i = ans.size();
+    while (i < n)
+    {
+        ans.pb(0);
+        i++;
+    }
+
+    printArr(ans);
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    clock_t z = clock();
+
+    int t = 0;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        SUM = 0;
+    }
+    return 0;
+}
